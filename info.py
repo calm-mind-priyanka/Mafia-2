30fefe2527f9fc9e95028b92c26c46dcad480fa8
calm-mind-priyanka <priyankagogoi1241@gmail.com>
2026-05-15 14:36:34 +0530

    Update info.py

--- info.py
+++ info.py
@@ -45,11 +45,11 @@ LOG_API_CHANNEL = int(environ.get("LOG_API_CHANNEL", "-1002433610423"))
 LOG_VR_CHANNEL = int(environ.get("LOG_VR_CHANNEL", "-1002433610423"))
 
 # MongoDB
-DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://babluboss:babluboss@cluster0.y04tdu1.mongodb.net/?appName=Cluster0")
+DATABASE_URI = environ.get("DATABASE_URI", "mongodb+srv://lastmongo:lastmongo@cluster0.skfurxv.mongodb.net/?appName=Cluster0")
 DATABASE_NAME = environ.get("DATABASE_NAME", "sdyimdx")
 
 # Files index database url
-FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://sandyboss:sandyboss@cluster0.msii66v.mongodb.net/?appName=Cluster0")
+FILES_DATABASE = environ.get("FILES_DATABASE", "mongodb+srv://lastmongo:lastmongo@cluster0.skfurxv.mongodb.net/?appName=Cluster0")
 COLLECTION_NAME = environ.get("COLLECTION_NAME", "sdyimdx")
 
 # Other Channel's
